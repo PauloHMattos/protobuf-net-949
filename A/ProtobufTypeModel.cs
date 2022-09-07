c3c2c43764e9572c21940893e47f3644cc282bc6
PM Paulo Mattos <pmattos@inoa.com.br>
2022-09-07 16:00:36 -0300

    Add project files.

---
+++ A/ProtobufTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ProtoBuf.Meta;
+
+namespace A {
+    public class ProtobufTypeModel {
+        private readonly RuntimeTypeModel _typeModel;
+
+        public ProtobufTypeModel(RuntimeTypeModel typeModel) {
+            _typeModel = typeModel;
+        }
+
+        public string GetSchemaWorking(Type type) => _typeModel.GetSchema(type);
+        public string GetSchemaBroken(Type type) => _typeModel.GetSchema(type, ProtoSyntax.Proto2);
+    }
+}
---
+++ Application/Program.cs
@@ -0,0 +1,26 @@
+﻿using A;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace Application {
+    internal class Program {
+        static void Main(string[] args) {
+            var protobufTypeModel = new ProtobufTypeModel(RuntimeTypeModel.Default);
+
+            // Works - proto3
+            Console.WriteLine(protobufTypeModel.GetSchemaWorking(typeof(ExampleModel)));
+            // Also works - proto2
+            Console.WriteLine(RuntimeTypeModel.Default.GetSchema(typeof(ExampleModel), ProtoSyntax.Proto2));
+            // MissingMethodException
+            Console.WriteLine(protobufTypeModel.GetSchemaBroken(typeof(ExampleModel)));
+        }
+    }
+
+    [ProtoContract]
+    public class ExampleModel {
+        [ProtoMember(1)]
+        public int Integer { get; set; }
+        [ProtoMember(2)]
+        public string Str { get; set; }
+    }
+}
